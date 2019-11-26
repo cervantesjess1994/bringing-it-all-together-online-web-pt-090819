650a5bd46b51dee98775216d0d460c866c2172e5
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-11-26 10:23:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,8 @@ class Dog
         SELECT * FROM dogs
         WHERE id = ?
       SQL
-      DB[:conn]execute.(sql,id). map do |row|
+      DB[:conn]execute.(sql, id).map do |row|
         self.new_from_db(row)
       end.first
+    end
 end
